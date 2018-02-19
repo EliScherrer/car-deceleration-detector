6255f9d38cfd8c05d9790860ce52efa93c0fa78b
Samuel Carbone <scarbon@purdue.edu>
2018-02-19 18:41:04 -0500

    Initial Commit

---
+++ ARSceneTests/ARSceneTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARSceneTests.swift
+//  ARSceneTests
+//
+//  Created by Samuel Carbone on 2/19/18.
+//  Copyright © 2018 Samuel Carbone. All rights reserved.
+//
+
+import XCTest
+@testable import ARScene
+
+class ARSceneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
