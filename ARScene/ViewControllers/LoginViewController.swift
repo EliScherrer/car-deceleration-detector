5b33793e37dfaaf56d29aaf511455682ab08821d
Samuel Carbone <scarbon@purdue.edu>
2018-02-19 20:03:44 -0500

    Basic login

---
+++ ARScene/ViewControllers/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  ARScene
+//
+//  Created by Samuel Carbone on 2/19/18.
+//  Copyright © 2018 Samuel Carbone. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: self.userEmail.text!, password: self.userPassword.text!, completion: {(user,error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        })
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        Auth.auth().createUser(withEmail: self.userEmail.text!, password: self.userPassword.text!) { (user, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ARScene' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ARScene
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  target 'ARSceneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ARSceneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
